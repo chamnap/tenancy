b46fece7b4c50aae7fb13803cf5f9ce5b9a2efd1
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-02-05 16:56:28 +0700

    add more examples on resource_spec.rb

--- spec/lib/resource_spec.rb
+++ spec/lib/resource_spec.rb
@@ -6,8 +6,6 @@ if defined?(ActiveRecord)
     let(:panpage) { Portal.create(id: 2, domain_name: "panpages.my") }
     let(:yoolk)   { Portal.create(id: 3, domain_name: "yoolk.com") }
 
-    before(:each) { RequestStore.store[:"Portal.current"] = nil }
-
     it "set current with instance" do
       Portal.current = camyp
 
@@ -48,6 +46,56 @@ if defined?(ActiveRecord)
 
     it "#with_scope without block" do
       expect { Portal.with_tenant(yoolk) }.to raise_error(ArgumentError)
+    end
+  end
+end
+
+if defined?(Mongoid)
+  describe "Tenancy::Resource" do
+    let(:camyp)   { Mongo::Portal.create(domain_name: "yp.com.kh") }
+    let(:panpage) { Mongo::Portal.create(domain_name: "panpages.my") }
+    let(:yoolk)   { Mongo::Portal.create(domain_name: "yoolk.com") }
+
+    it "set current with instance" do
+      Mongo::Portal.current = camyp
+
+      Mongo::Portal.current.should == camyp
+      RequestStore.store[:"Mongo::Portal.current"].should == camyp
+    end
+
+    it "set current with id" do
+      Mongo::Portal.current = panpage.id
+
+      Mongo::Portal.current.should == panpage
+      RequestStore.store[:"Mongo::Portal.current"].should == panpage
+    end
+
+    it "set current with nil" do
+      Mongo::Portal.current = panpage
+      Mongo::Portal.current = nil
+
+      Mongo::Portal.current.should == nil
+      RequestStore.store[:"Mongo::Portal.current"].should == nil
+    end
+
+    it "#current_id" do
+      Mongo::Portal.current = yoolk
+
+      Mongo::Portal.current_id.should == yoolk.id
+    end
+
+    it "#with_scope with block" do
+      Mongo::Portal.current.should == nil
+
+      Mongo::Portal.with_tenant(yoolk) do
+        Mongo::Portal.current.should == yoolk
+      end
+
+      Mongo::Portal.current.should == nil
+    end
+
+    it "#with_scope without block" do
+      expect { Mongo::Portal.with_tenant(yoolk) }.to raise_error(ArgumentError)
     end
   end
 end
